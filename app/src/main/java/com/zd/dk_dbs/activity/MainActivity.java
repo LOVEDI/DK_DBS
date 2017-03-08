cb5c0007eee15024d187895684038db8fcf41647
ZHAODI <893959540@example.com>
2017-03-08 11:00:33 +0800

    第一次提交

--- app/src/main/java/com/zd/dk_dbs/activity/MainActivity.java
+++ app/src/main/java/com/zd/dk_dbs/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zd.dk_dbs;
+package com.zd.dk_dbs.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zd.dk_dbs.R;
 
 public class MainActivity extends AppCompatActivity {
 
